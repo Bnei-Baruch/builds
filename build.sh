4b9e0abb942288a3799e497f40b1d1dede9ef786
yosefy <yosefy@users.noreply.github.com>
2024-03-03 12:09:55 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /usr/src
+git clone https://github.com/Bnei-Baruch/builds.git
+
+BUILD_CONTAINER=$1
+ARTIFACT_NAME=$2
+ARTIFACT_VER=$3
+
+docker run \
+-v /usr/src:/usr/src \
+-v /artifacts:/artifacts \
+-e ARTIFACT_VER="$ARTIFACT_VER" \
+${BUILD_CONTAINER} \
+/bin/bash /usr/src/nginx/build_${ARTIFACT_NAME}
