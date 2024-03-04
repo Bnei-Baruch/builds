b103a28815f89c8ddea9addd751801ac49c27407
yosefy <yosefy@users.noreply.github.com>
2024-03-04 04:52:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# cd /usr/src
-# git clone https://github.com/Bnei-Baruch/builds.git
 
 BUILD_CONTAINER=$1
 ARTIFACT_NAME=$2
 ARTIFACT_VER=$3
 
 docker run \
--v /usr/src:/usr/src \
+-v /usr/src/builds/${ARTIFACT_NAME}:/usr/src/builds/${ARTIFACT_NAME} \
 -v /artifacts:/artifacts \
 -e ARTIFACT_VER="$ARTIFACT_VER" \
+-e BUILD_CONTAINER="$BUILD_CONTAINER" \
 ${BUILD_CONTAINER} \
-/bin/bash /usr/src/${ARTIFACT_NAME}/build_${ARTIFACT_NAME}.sh
+/bin/bash /usr/src/builds/${ARTIFACT_NAME}/build_${ARTIFACT_NAME}.sh
